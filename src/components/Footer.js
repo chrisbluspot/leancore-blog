af4e30a4b290948ced8210c5ae22b5c93182a056
Christian <christian@bluspot.us>
2020-08-04 11:49:39 -0500

    Footer updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
-import logo from '../img/logo.svg'
 import facebook from '../img/social/facebook.svg'
 import instagram from '../img/social/instagram.svg'
 import twitter from '../img/social/twitter.svg'
@@ -10,38 +9,25 @@ import vimeo from '../img/social/vimeo.svg'
 const Footer = class extends React.Component {
   render() {
     return (
-      <footer className="footer has-background-black has-text-white-ter">
+      <footer 
+        className="footer" 
+        style={{ 
+          borderTop: '2px solid lightgray',
+          paddingBottom: '5%'
+        }}
+      >
         <div className="content has-text-centered">
-          <img
-            src={logo}
-            alt="Kaldi"
-            style={{ width: '14em', height: '10em' }}
-          />
-        </div>
-        <div className="content has-text-centered has-background-black has-text-white-ter">
-          <div className="container has-background-black has-text-white-ter">
+          <div className="container">
             <div style={{ maxWidth: '100vw' }} className="columns">
               <div className="column is-4">
                 <section className="menu">
                   <ul className="menu-list">
-                    <li>
-                      <Link to="/" className="navbar-item">
-                        Home
-                      </Link>
+                    <li style={{ marginLeft: '3%', fontWeight: 600 }}>
+                      DE INTERÉS
                     </li>
                     <li>
-                      <Link className="navbar-item" to="/about">
-                        About
-                      </Link>
-                    </li>
-                    <li>
-                      <Link className="navbar-item" to="/products">
-                        Products
-                      </Link>
-                    </li>
-                    <li>
-                      <Link className="navbar-item" to="/contact/examples">
-                        Form Examples
+                      <Link to="/" className="navbar-item" style={{ color: '#00AFDC' }}>
+                        Política de privacidad
                       </Link>
                     </li>
                     <li>
@@ -50,6 +36,7 @@ const Footer = class extends React.Component {
                         href="/admin/"
                         target="_blank"
                         rel="noopener noreferrer"
+                        style={{ color: '#00AFDC' }}
                       >
                         Admin
                       </a>
@@ -60,21 +47,57 @@ const Footer = class extends React.Component {
               <div className="column is-4">
                 <section>
                   <ul className="menu-list">
-                    <li>
-                      <Link className="navbar-item" to="/blog">
-                        Latest Stories
-                      </Link>
+                    <li style={{ marginLeft: '3%', fontWeight: 600 }}>
+                      SÍGUENOS
                     </li>
                     <li>
-                      <Link className="navbar-item" to="/contact">
-                        Contact
-                      </Link>
+                      <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+                        <a title="instagram" href="https://instagram.com">
+                          <img
+                            src={instagram}
+                            alt="Instagram"
+                            style={{ width: '1em', height: '1em', }}
+                          />
+                        </a>
+                        <Link className="navbar-item" to="/blog" style={{ color: '#00AFDC' }}>
+                          Instagram
+                        </Link>
+                      </div>
+                    </li>
+                    <li>
+                      <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+                        <a title="facebook" href="https://facebook.com">
+                          <img
+                            src={facebook}
+                            alt="Facebook"
+                            style={{ width: '1em', height: '1em' }}
+                          />
+                        </a>
+                        <Link className="navbar-item" to="/contact" style={{ color: '#00AFDC' }}>
+                          Facebook
+                        </Link>
+                      </div>
                     </li>
                   </ul>
                 </section>
               </div>
               <div className="column is-4 social">
-                <a title="facebook" href="https://facebook.com">
+                <ul className="menu-list">
+                  <li style={{ marginLeft: '3%', fontWeight: 600 }}>
+                    SUSCRÍBETE A NUESTRO NEWSLETTER
+                  </li>
+                </ul>
+                <p 
+                  style={{
+                    color: 'black',
+                    opacity: 0.75,
+                    textAlign: 'initial',
+                    marginLeft: '11%'
+                  }}
+                >
+                  Te contamos de nuevos parqueaderos y oportunidades de negocio
+                </p>
+                {/* <a title="facebook" href="https://facebook.com">
                   <img
                     src={facebook}
                     alt="Facebook"
@@ -102,7 +125,7 @@ const Footer = class extends React.Component {
                     alt="Vimeo"
                     style={{ width: '1em', height: '1em' }}
                   />
-                </a>
+                </a> */}
               </div>
             </div>
           </div>
